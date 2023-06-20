68d9f394e7441bd7495e58c54c3c73d9664a4739
RezaHemadi <r.hemadi@gmail.com>
2023-06-20 13:46:01 +0330

    Initial Commit

---
+++ OptCuts/OptCuts.h
@@ -0,0 +1,18 @@
+//
+//  OptCuts.h
+//  OptCuts
+//
+//  Created by Reza on 6/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OptCuts.
+FOUNDATION_EXPORT double OptCutsVersionNumber;
+
+//! Project version string for OptCuts.
+FOUNDATION_EXPORT const unsigned char OptCutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptCuts/PublicHeader.h>
+
+
